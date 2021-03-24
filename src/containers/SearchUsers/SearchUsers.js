6c35b88fdc8c17f37d02a030fec1a48ad88eaafb
Juan <jschaper@hotmail.co.za>
2021-03-24 14:32:03 +0200

    requestContact() code implemented.

--- src/containers/SearchUsers/SearchUsers.js
+++ src/containers/SearchUsers/SearchUsers.js
@@ -13,6 +13,7 @@ import styles from './SearchUsers.module.css';
 const SearchUsers = () => {
 	const [userInput, setUserInput] = useState('');
 	const [results, setResults] = useState([]);
+	const [reqSentMsg, setReqSentMsg] = useState();
 	const dispatch = useDispatch();
 	const user = useSelector(state => state.user);
 	const { sendRequest, isLoading, error, clearError } = useHttpClient();
@@ -41,60 +42,84 @@ const SearchUsers = () => {
 		}
 	};
 
-	const addContactHandler = async id => {
-		console.log('Request sent...')
+	const requestContactHandler = async (e, id) => {
+		e.preventDefault();
+		try {
+			const body = JSON.stringify({ requestReceiverId: id });
+			const response = await sendRequest(
+				`${process.env.REACT_APP_BACKEND_URL}/contact/request-contact`,
+				'POST',
+				body,
+				{
+					Authorization: `Bearer ${user.token}`,
+					'Content-Type': 'application/json'
+				}
+			);
+			setReqSentMsg(response.message);
+		} catch (err) {
+			console.log(err);
+		}
 	};
 
-	const displayResults =
-		!isLoading && results.length === 0 ? (
-			<p className={styles.Notice}>No search results to display...</p>
-		) : (
-			results.map(user => (
-				<Card
-					key={user.id}
-					userName={user.name}
-					userId={user.id}
-					avatarUrl={user.avatarUrl}>
-					<br />
-					<Button
-						value={'Add Contact'}
-						cssForButton={['Btn-Dark', 'Btn-Wide']}
-						clicked={() => addContactHandler(user.id)}
-					/>
-				</Card>
-			))
-		);
+	const clearModalHandler = () => {
+		clearError();
+		setReqSentMsg(undefined);
+	};
 
 	return (
 		<React.Fragment>
-			{error && (
+			{(error || reqSentMsg) && (
 				<Modal
-					visible={error}
-					header='Error...'
-					btnClicked={() => clearError()}>
-					{error}
+					visible={error || reqSentMsg}
+					header={error ? 'Whoops...' : 'Success...'}
+					btnClicked={() => clearModalHandler()}>
+					{error ? error : reqSentMsg}
 				</Modal>
 			)}
-			{error && <Backdrop visible={error} clicked={() => clearError()} />}
+			{(error || reqSentMsg) && (
+				<Backdrop
+					visible={error || reqSentMsg}
+					clicked={() => clearModalHandler()}
+				/>
+			)}
 			<h1 className={styles.Heading}>Search Users</h1>
 			<div className={styles.Container}>
-				<form className={styles.SearchForm}>
-					<input
-						className={styles.SearchTermInput}
-						type='text'
-						placeholder='Type a username here...'
-						value={userInput}
-						onChange={e => setUserInput(e.target.value)}
-					/>
-					<Button
-						value='Search'
-						cssForButton={['Btn-Dark', 'Btn-Medium']}
-						clicked={e => searchHandler(e)}
-					/>
-				</form>
-				{isLoading && <LoadingIndicator size='LoaderLarge' />}
+				{isLoading ? (
+					<LoadingIndicator size='LoaderLarge' />
+				) : (
+					<form className={styles.SearchForm}>
+						<input
+							className={styles.SearchTermInput}
+							type='text'
+							placeholder='Type a username here...'
+							value={userInput}
+							onChange={e => setUserInput(e.target.value)}
+						/>
+						<Button
+							value='Search'
+							cssForButton={['Btn-Dark', 'Btn-Medium']}
+							clicked={e => searchHandler(e)}
+						/>
+					</form>
+				)}
 				<div className={styles.SearchResults}>
-					{!isLoading && displayResults}
+					{!isLoading && !results.length && (
+						<p className={styles.Notice}>No search results to display...</p>
+					)}
+					{results.map(user => (
+						<Card
+							key={user.id}
+							userName={user.name}
+							userId={user.id}
+							avatarUrl={user.avatarUrl}>
+							<br />
+							<Button
+								value='Request Contact'
+								cssForButton={['Btn-Dark', 'Btn-Wide']}
+								clicked={e => requestContactHandler(e, user.id)}
+							/>
+						</Card>
+					))}
 				</div>
 			</div>
 		</React.Fragment>
